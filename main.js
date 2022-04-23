7e27471ad59671d842ce136f3d8344da91aa2a1d
BeatrizOliveira16 <62113636+BeatrizOliveira16@users.noreply.github.com>
2022-04-23 18:25:50 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,23 +1,21 @@
-let scenes = [];
+let scenes = []
 scenes.push(FirstScene);
 scenes.push(CreditsScene);
 scenes.push(InfoScene);
 scenes.push(PracticeScene);
 scenes.push(UnderstandScene);
 
-
-
 const config = {
   type: Phaser.AUTO,
   scale: {
      mode: Phaser.Scale.ScaleModes.FIT,
      autoCenter: Phaser.Scale.CENTER_BOTH,
-     width: 2048,
-     height: 1200
+     width: 2048, 
+     height: 1190
     },
-  backgroundColor : "#a9e6ff;",
+  backgroundColor : "#ffffff",
   parent : 'game',
-  scene : scenes 
+  scene : scenes
 };
 
 var game = new Phaser.Game(config);
