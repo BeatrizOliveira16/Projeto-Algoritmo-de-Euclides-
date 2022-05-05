43615428a8cd52606b1f3920d311f2286a6c21c9
Beatriz Oliveira <62113636+BeatrizOliveira16@users.noreply.github.com>
2022-05-05 22:09:21 +0100

    Add files via upload

--- Tabela.js
+++ Tabela.js
@@ -1,74 +1,74 @@
 class Tabela{
     constructor(dividendo, divisor, quociente, resto, posicao){
-        this.dividendo = dividendo
-        this.divisor = divisor
-        this.quociente = quociente
-        this.resto = resto
-        this.posicao = posicao
+        this.dividendo = dividendo;
+        this.divisor = divisor;
+        this.quociente = quociente;
+        this.resto = resto;
+        this.posicao = posicao;
     }
 /*
     get dividendo(){
-        return this.dividendo
+        return this.dividendo;
     }
     
     get divisor(){
-        return this.divisor
+        return this.divisor;
     }
 
     get resto(){
-        return this.resto
+        return this.resto;
     }
 
     get quociente(){
-        return this.quociente
+        return this.quociente;
     }
 
     get posicao(){
-        return this.posicao
+        return this.posicao;
     }
 
     set dividendo(val){
-        this.dividendo = val
+        this.dividendo = val;
     }
     
     set divisor(val){
-        this.divisor = val
+        this.divisor = val;
     }
 
     set resto(val){
-        this.resto = val
+        this.resto = val;
     }
 
     set quociente(val){
-        this.quociente = val
+        this.quociente = val;
     }
 
      set posicao(val){
-        this.posicao = val
+        this.posicao = val;
     }
-*/
+    */
 }
 
 class Tabelas{
     constructor(){
-        this.tabelas=[]
+        this.tabelas=[];
     }
 
     newTabelas(dividendo, divisor, quociente, resto, posicao){
-        let p = new Tabela(dividendo, divisor, quociente, resto, posicao)
-        this.tabelas.push(p)
-        return p
+        let p = new Tabela(dividendo, divisor, quociente, resto, posicao);
+        this.tabelas.push(p);
+        return p;
     }
 
     get allTabelas(){
-        return this.tabelas
+        return this.tabelas;
     }
 
     get numberofTabelas(){
-        return this.tabelas.length
+        return this.tabelas.length;
     }
 
     get dimension_width(){
-        return 68.84 * (this.tabelas.length)  + 157.47
+        return 68.84 * (this.tabelas.length)  + 157.47;
     }
 }
