abd3d5c702c4b4c7088fa083d660e5e8800fa6cf
CatarinaQuintas <62112630+CatarinaQuintas@users.noreply.github.com>
2022-06-03 21:06:32 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -4,8 +4,15 @@ scenes.push(FirstScene);
 scenes.push(CreditsScene);
 scenes.push(InfoScene);
 scenes.push(PracticeScene);
-scenes.push(Info2Scene);
 scenes.push(UnderstandScene);
+scenes.push(UnderstandScene1);
+scenes.push(UnderstandScene2);
+scenes.push(UnderstandScene3);
+scenes.push(UnderstandScene4);
+scenes.push(UnderstandScene5);
+scenes.push(UnderstandScene6);
+scenes.push(UnderstandScene7);
+scenes.push(UnderstandScene8);
 
 const config = {
   type: Phaser.AUTO,
